14cabd8a2a4d8f765c0d8c76a3fab799f2954f7c
LajithFrancis <lajithf@gmail.com>
2024-07-27 00:08:45 +0530

    "Updated Footer component to use StaticText imports for footer message, project name, and footer caption."

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,13 @@
 import './styles.css'
-
+import StaticText from '@/config/static'
 export default function Footer() {
     return (
         <footer className="fixed bottom-0 w-full p-4">
             <div className="flex flex-col text-center">
-                <p className="bg-green-500 py-2">366,681,625 stock photos, 360 panoramic images, vectors and videos</p>
+                <p className="bg-green-500 py-2">{StaticText.footerMessage}</p>
                 <div className="bg-black text-white py-4">
-                    <h2>My Image Search</h2>
-                    <p>Changing the world one image at a time</p>
+                    <h2>{StaticText.projectName}</h2>
+                    <p>{StaticText.footerCaption}</p>
                 </div>
             </div>
         </footer>
---
+++ src/config/static.tsx
@@ -0,0 +1,7 @@
+const StaticText = {
+    footerMessage: '366,681,625 stock photos, 360° panoramic images, vectors and videos',
+    footerCaption: 'Changing the world one image at a time',
+    projectName: 'My Image Search'
+}
+
+export default StaticText;
