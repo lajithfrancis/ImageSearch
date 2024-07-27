04cb6d6be281c2aa354568993173cd346fe682b3
LajithFrancis <lajithf@gmail.com>
2024-07-27 22:55:50 +0530

    "Updated ImageCard component to add new font awesome icons, modified icon styles and added hover effects to photographer name and icon list."

--- src/components/ImageGallery/ImageCard.tsx
+++ src/components/ImageGallery/ImageCard.tsx
@@ -1,21 +1,21 @@
 import { Photo } from "@/app/interfaces/SearchList"
-import { faCartShopping, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faDownload, faPlus, faImages } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Image from 'next/image';
 
 export default function ImageCard({ index, item }: {
     index: number,
     item: Photo
 }) {
+    const iconStyleClasses = 'relative p-4 rounded bg-gray-800 text-white icon'
     return (
         <div key={index} className="masonry-brick">
             <img src={item.src.large} alt={item.alt} className="image" />
-            <p className="text-sm text-white relative left-2 bottom-8 w-fit"><b>Photographer: </b>{item.photographer}</p>
-            <div>
-                <FontAwesomeIcon className="relative p-4 rounded bg-gray-800 text-green-500" icon={faCartShopping} />
-                <FontAwesomeIcon className="relative p-4 rounded bg-gray-800 text-green-500" icon={faDownload} />
-                <FontAwesomeIcon className="relative p-4 rounded bg-gray-800 text-green-500" icon={faCartShopping} />
-                <FontAwesomeIcon className="relative p-4 rounded bg-gray-800 text-green-500" icon={faCartShopping} />
+            <p className="text-sm text-white relative left-2 bottom-8 w-fit name"><b>Photographer: </b>{item.photographer}</p>
+            <div className="icon_list">
+                <FontAwesomeIcon className={iconStyleClasses} icon={faCartShopping} />
+                <FontAwesomeIcon className={iconStyleClasses} icon={faDownload} />
+                <FontAwesomeIcon className={iconStyleClasses} icon={faPlus} />
+                <FontAwesomeIcon className={iconStyleClasses} icon={faImages} />
             </div>
         </div>
     )
