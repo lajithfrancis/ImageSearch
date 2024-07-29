8cb3fe70b0753ef3866a4b05d5ac00797de35e3f
LajithFrancis <lajithf@gmail.com>
2024-07-29 11:46:26 +0530

    "Added react-icons dependency and updated Nav component to use react-icons"

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,5 +1,7 @@
 import { faHeart, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaRegHeart } from 'react-icons/fa';
+import { HiOutlineShoppingCart } from "react-icons/hi";
 import './styles.css'
 
 export const Nav = () => {
@@ -7,8 +9,10 @@ export const Nav = () => {
         <nav className='flex items-center bg-black text-white p-4'>
           <h1 className="text-4xl font-bold text-start flex-1">My Image Search</h1>
           <ul className='flex gap-2'>
-            <li><FontAwesomeIcon icon={faHeart}/> Lightboxes</li>
-            <li><FontAwesomeIcon icon={faCartShopping}/> Cart</li>
+          <li><FaRegHeart className='size-6' /> Lightboxes</li>
+          <li><HiOutlineShoppingCart className='size-6' /> Cart</li>
+          {/* <li><FontAwesomeIcon icon={faHeart}/> Lightboxes</li>
+            <li><FontAwesomeIcon icon={faCartShopping}/> Cart</li> */}
             <li>Sign in</li>
           </ul>
         </nav>
